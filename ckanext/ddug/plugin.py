8784cf4b04f891d98feda350b7577f8c487c9b09
dan mulindwa <dmulindwa@gmail.com>
2017-01-19 20:12:45 +0300

    Request

--- ckanext/ddug/plugin.py
+++ ckanext/ddug/plugin.py
@@ -4,6 +4,7 @@ import ckan.plugins.toolkit as toolkit
 
 class DdugPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IRoutes, inherit=True)
 
     # IConfigurer
 
@@ -11,3 +12,7 @@ class DdugPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'ddug')
+
+    def after_map(self, m):
+        m.connect('feedbackProv1', '/feedbackProv1', controller='ckanext.ddug.controller:DdugController', action='feedbackProv1')
+        return m
